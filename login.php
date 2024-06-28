85da1b40dd20d24e571ad6ae20519eb015ba7a65
Samuele-Chioda <samuelechioda@gmail.com>
2024-06-28 15:13:29 +0200

    aggiunta Array

---
+++ login.php
@@ -0,0 +1,17 @@
+<?php
+
+$utenti = [
+    [
+        'nome' => 'utenteUno',
+        'password' => 'passwordUno'
+    ],
+    [
+        'nome' => 'utenteDue',
+        'password' => 'passwordDue'
+    ],
+    [
+        'nome' => 'utenteTre',
+        'password' => 'passwordTre'
+    ]
+];
+
