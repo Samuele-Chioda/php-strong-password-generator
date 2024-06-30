98ba45e4e20167833871eef0be85603a703c45ca
Samuele-Chioda <samuelechioda@gmail.com>
2024-07-01 00:47:10 +0200

    Aggiunto  reindirizzamento a index.php e messaggio di benvenuto in caso di accesso riuscito. Aggiunta style bottone

--- index.php
+++ index.php
@@ -1,11 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Session Login</title>
 </head>
+
 <body>
-    <?php   ?>
+    <div>
+        <h1>Accesso Consentito!</h1>
+        <h2>
+            <?php
+
+            session_start();
+
+            // isset ontrolla che la variabile username sia impostata!!!
+            if (isset($_SESSION['username'])) {
+            } else {
+                // Se non è impostata, rimando utente a pagina di login
+                header('Location: login.php');
+                exit();
+            }
+            echo "Benvenuto, " . $_SESSION['username'] . '!';
+            
+            ?>
+        </h2>
+    </div>
 </body>
+
 </html>
+
+<style>
+    body {
+        background-color: red;
+    }
+
+    div {
+        padding: 5rem;
+        background-color: lightblue;
+        display: flex;
+        justify-content: center;
+        height: 100vh;
+        font-size: 2rem;
+        font-weight: bolder;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
--- login.php
+++ login.php
@@ -56,13 +56,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <label for="password">Password:</label>
             <input type="password" id="password" name="password" required>
             <br>
-            <button type="submit">Login</button>
+            <button class="button-85 submit" role="button">Login</button>
         </form>
         <?php
         //messaggio di errore se il metodo di richiesta è POST e $loginSuccess è false
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if (!$loginSuccess) {
-                echo '<p>Accesso negato. Riprova.</p>';
+                echo '<p>Accesso negato. <br> Riprova.</p>';
             }
         }
         ?>
@@ -70,3 +70,98 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </body>
 
 </html>
+
+<style>
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    div {
+        padding: 5rem;
+        background-color: lightblue;
+        display: flex;
+        justify-content: center;
+        height: 100vh;
+    }
+
+    p {
+        margin: 1rem;
+        color: red;
+        font-size: 1.2rem;
+        font-weight: bold;
+    }
+
+    form {
+        font-size: 2rem;
+        font-weight: bolder;
+    }
+
+    .button-85 {
+        padding: 0.6em 2em;
+        border: none;
+        outline: none;
+        color: rgb(255, 255, 255);
+        background: #111;
+        cursor: pointer;
+        position: relative;
+        z-index: 0;
+        border-radius: 10px;
+        user-select: none;
+        -webkit-user-select: none;
+        touch-action: manipulation;
+    }
+
+    .button-85:before {
+        content: "";
+        background: linear-gradient(45deg,
+                #ff0000,
+                #ff7300,
+                #fffb00,
+                #48ff00,
+                #00ffd5,
+                #002bff,
+                #7a00ff,
+                #ff00c8,
+                #ff0000);
+        position: absolute;
+        top: -2px;
+        left: -2px;
+        background-size: 400%;
+        z-index: -1;
+        filter: blur(5px);
+        -webkit-filter: blur(5px);
+        width: calc(100% + 4px);
+        height: calc(100% + 4px);
+        animation: glowing-button-85 20s linear infinite;
+        transition: opacity 0.3s ease-in-out;
+        border-radius: 10px;
+    }
+
+    @keyframes glowing-button-85 {
+        0% {
+            background-position: 0 0;
+        }
+
+        50% {
+            background-position: 400% 0;
+        }
+
+        100% {
+            background-position: 0 0;
+        }
+    }
+
+    .button-85:after {
+        z-index: -1;
+        content: "";
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background: #222;
+        left: 0;
+        top: 0;
+        border-radius: 10px;
+    }
+</style>
